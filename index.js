93c2b3b5b28cce41e673cdea0f11450b630c1a4f
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2023-02-27 19:30:37 +0600

    set a api route to get card data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const port = process.env.PORT || 5000;
+const cardData = require('./CardData.json')
 require('dotenv').config()
 const app = express()
 
@@ -10,6 +11,12 @@ app.get('/',(req,res)=>{
     res.send('Server is running')
 })
 
+app.get('/data',(req,res)=>{
+    res.send({
+        success: true,
+        data: cardData
+    })
+})
 
 app.listen(port,()=>{
     console.log("Server is running on port",port)
